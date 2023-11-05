3790073ac97d938aa71a1b0fff7fa189e624f167
bell-person-ii <leeji88460@gmail.com>
2023-11-05 14:10:37 +0900

    repository and domain are implemented

---
+++ jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,26 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Member;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+@Transactional
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public Long save(Member member){
+        em.persist(member);
+        return (member.getId());
+    }
+
+    public Member find(Long id){
+        return em.find(Member.class,id);
+    }
+
+}
