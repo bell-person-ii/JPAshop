0fcd912143d2b84dc1997f809fd5ac7fc57383bc
bell-person-ii <leeji88460@gmail.com>
2023-11-09 18:01:34 +0900

    Member Repository 구현

--- jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -6,6 +6,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 
 @Repository
 public class MemberRepository {
@@ -18,8 +19,21 @@ public class MemberRepository {
         return (member.getId());
     }
 
+    // 단건 조회 find(타입,key)
     public Member find(Long id){
         return em.find(Member.class,id);
+    }
+
+    // 다중 조회 jpql 사용
+    public List<Member> findAll(){
+        List<Member> result = em.createQuery("select m from Member m", Member.class).getResultList();
+        return result;
+    }
+
+    public List<Member> findByName(String name){
+        List<Member> result  = em.createQuery("select m from Member m  where m.name = :name", Member.class)
+                .setParameter("name",name).getResultList();
+        return result;
     }
 
 }
